c6837bf2161a2e0297164a9f662472d44003da90
Siddharth Chopra <sid6327@gmail.com>
2017-08-13 23:51:21 +0530

    Adding surface view and mouse control.

--- SocketIO/app/src/main/java/com/example/siddharth/socketio/MainActivity.kt
+++ SocketIO/app/src/main/java/com/example/siddharth/socketio/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.siddharth.socketio
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.SurfaceView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,10 +15,13 @@ import java.net.URISyntaxException
 
 class MainActivity : AppCompatActivity() {
 
-    val socket = try { IO.socket("http://192.168.1.13:3000") } catch (e: URISyntaxException) { null }
+    val socket = try { IO.socket("http://192.168.1.17:3000") } catch (e: URISyntaxException) { null }
     var textView: TextView? = null
     var send: Button? = null
     var inputText: EditText? = null
+    var surface: SurfaceView? = null
+    var x: Float = 0.0f
+    var y: Float = 0.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +29,34 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.textView) as? TextView
         send = findViewById(R.id.button) as? Button
         inputText = findViewById(R.id.editText) as? EditText
-        setSendListener()
+        surface = findViewById(R.id.surfaceView) as? SurfaceView
         connectSocket()
+        addSurfaceListener()
+        setSendListener()
+    }
+
+//    fun
+
+    fun addSurfaceListener() {
+        surface?.setOnTouchListener { v, event ->
+            textView?.text = "X = $x, Y = $y"
+            val action = event.action
+            when (action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_DOWN -> {
+                    x = event.rawX
+                    y = event.rawY
+//                    textView?.text = "X = $x, Y = $y"
+                    socket?.emit("mouseEvent", x.toLong())
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    x = event.rawX
+                    y = event.rawY
+//                    textView?.text = "X = $x, Y = $y"
+                    socket?.emit("mouseEvent", x.toLong())
+                }
+            }
+            true
+        }
     }
 
     fun setSendListener() {
